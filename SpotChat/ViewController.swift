a53ff86995770ddb85646a2313c4b69c7d0bd82e
dsungc1111 <eotjd9808@naver.com>
2024-10-24 19:42:39 +0900

    Initial Commit

---
+++ SpotChat/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  SpotChat
+//
+//  Created by 최대성 on 10/24/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        view.backgroundColor = .red
+    }
+
+
+}
+
